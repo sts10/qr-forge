02f3b46d6ee3f246d7bc7ca526fb787a5814ddd1
sts10 <sschlinkert@gmail.com>
2019-05-02 22:03:04 -0400

    new initial commit

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+extern crate qrcode;
+extern crate rpassword;
+use qrcode::QrCode;
+use std::io;
+
+fn main() {
+    let key =
+        rpassword::read_password_from_tty(Some("Enter the secret:\n")).expect("Error reading key");
+
+    println!("Enter name of service:");
+    let service = gets().unwrap();
+    println!("Enter user name:");
+    let username = gets().unwrap();
+
+    let otpauth_uri = format!(
+        "otpauth://totp/{}:@{}?secret={}&issuer={}",
+        service, username, key, service
+    );
+
+    display_qr_code(&otpauth_uri).expect("Couldn't display QR code");
+}
+
+fn display_qr_code(otpauth_uri: &str) -> Result<(), io::Error> {
+    let code = QrCode::new(otpauth_uri).unwrap();
+
+    let string = code
+        .render::<char>()
+        .quiet_zone(false)
+        .module_dimensions(2, 1)
+        .build();
+    println!("{}", string);
+    Ok(())
+}
+
+fn gets() -> io::Result<String> {
+    let mut input = String::new();
+    match io::stdin().read_line(&mut input) {
+        Ok(_n) => Ok(input.trim_end_matches('\n').to_string()),
+        Err(error) => Err(error),
+    }
+}
